080843dfab866caa97c1b41af2db0eeac20dffae
prio101 <miprio101@gmail.com>
2021-08-27 22:53:07 +0600

    setup: Added the Tailwindcss

--- assets/js/greeter.tsx
+++ assets/js/greeter.tsx
@@ -6,7 +6,7 @@ const Greeter: React.FC<GreeterProps> = (props: GreeterProps) => {
   const name = props.name;
   return (
     <section className="phx-hero">
-      <h1>Welcome to {name} with Typescript and React!</h1>
+      <h1 className="text-red-500 text-5xl font-bold text-center">Welcome to {name} with Typescript and React!</h1>
       <p>Peace-of-mind from prototype to production</p>
     </section>
   );
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -43,6 +43,7 @@ module.exports = (env, options) => {
           use: [
             MiniCssExtractPlugin.loader,
             'css-loader',
+            'postcss-loader',
             'sass-loader',
           ],
         }
