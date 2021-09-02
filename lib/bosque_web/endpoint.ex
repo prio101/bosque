9f8021ac838646654f86e14c214d080af4106705
prio101 <miprio101@gmail.com>
2021-09-03 00:15:59 +0600

    Deploy: Added the config for the Heroku

--- lib/bosque_web/endpoint.ex
+++ lib/bosque_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule BosqueWeb.Endpoint do
   ]
 
   socket "/socket", BosqueWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
